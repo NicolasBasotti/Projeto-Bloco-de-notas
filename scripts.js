4d618d1ec20ccc8b2237a1d76b74e598389fa191
Nícolas André Basotti <114263724+NicolasBasotti@users.noreply.github.com>
2026-06-17 11:41:10 -0300

    Add files via upload
    LocalStorage funcionando totalmente, alteração do input para melhor experiencia do usuário e começo de criação do botão editar

--- scripts.js
+++ scripts.js
@@ -5,15 +5,21 @@ let listaCompleta = document.getElementById("tarefas")
 
 let arrayTarefas = []
 
+let storage = localStorage;
+
+arrayTarefas = JSON.parse(storage.getItem("tarefasLista")) || []
+
 function cliqueBotao(){
     let texto = input.value.trim()
 
     if (!texto)
         return
-    else { 
-        arrayTarefas.push({texto, concluida: false})
-        mostrarNaTela()
-    }
+
+    arrayTarefas.push({texto, concluida: false})
+    atualizaTarefasStorage(arrayTarefas)
+    mostrarNaTela()
+    input.value = ""
+    input.focus()
 }
 
 function mostrarNaTela(){
@@ -26,17 +32,18 @@ function mostrarNaTela(){
         <li class="item-lista ${tarefaConcluida}"  data-indice="${indice}">
             <p class="item-tarefa ">${tarefa.texto}</p>
             <button class="button-item-tarefa botao-hover button-check-item">✓</button>
+            <button class="button-item-tarefa botao-hover button-editar-item">✎</button>
             <button class="button-item-tarefa botao-hover button-excluir-item">x</button>
         </li>
         `
     })
     
     listaCompleta.innerHTML = novaLista
-    
 }
 
 function limpaLista(){
     arrayTarefas = []
+    atualizaTarefasStorage(arrayTarefas)
     mostrarNaTela()
 }
 
@@ -52,11 +59,21 @@ function clicarItem(event){
         arrayTarefas[indiceTarefa].concluida = !arrayTarefas[indiceTarefa].concluida
     } else if (event.target.classList.contains("button-excluir-item")){
         arrayTarefas.splice(indiceTarefa, 1)
+        
     }
+
+    atualizaTarefasStorage(arrayTarefas)
     mostrarNaTela()
 }
     
+function atualizaTarefasStorage(arrayT){
+    storage.setItem("tarefasLista", JSON.stringify(arrayT))
+}
 
+function clicarEnter(event){
+    if (event.key === 'Enter')
+        cliqueBotao()
+}
 
 buttonAd.addEventListener("click", cliqueBotao)
 
@@ -64,7 +81,7 @@ buttonEx.addEventListener("click", limpaLista)
 
 listaCompleta.addEventListener("click", clicarItem)
 
+input.addEventListener("keydown", clicarEnter)
 
-
-
+mostrarNaTela()
 
